4e3342d0ed7418da50f0f355f330462b3bfb52be
Chrispy <cpetta@oaisd.org>
2020-09-28 12:51:13 -0400

    Create App.svelte

---
+++ App.svelte
@@ -0,0 +1,11 @@
+<script>
+import Cclicence from "./cclicence.svelte";
+</script>
+
+<Cclicence author="foo" authorURL="" workName="bar" workURL="" licenceType="zero" />
+<Cclicence authorURL="" titleOfWork="bar" workURL="" licenceType="by" />
+<Cclicence author="foo" authorURL="" workURL="www.wikipedia.org" licenceType="by-sa" />
+<Cclicence author="foo" authorURL="" workName="bar" workURL="" licenceType="by-nd" />
+<Cclicence author="foo" authorURL="" workName="bar" workURL="" licenceType="by-nc" />
+<Cclicence author="foo" authorURL="" workName="bar" workURL="" licenceType="by-nc-sa" />
+<Cclicence licenceType="by-nc-nd" />
